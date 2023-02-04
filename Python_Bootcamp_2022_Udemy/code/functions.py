552ebc10dc7fad02a163ed2debff6e29e3f64980
hard$hell <48847865+ericlikedis@users.noreply.github.com>
2023-02-04 18:21:17 +0000

    added more logic

--- Python_Bootcamp_2022_Udemy/code/functions.py
+++ Python_Bootcamp_2022_Udemy/code/functions.py
@@ -1,2 +1,21 @@
 # Functions 
-print('hello world')
+def name_of_function():
+    """
+    This is a docstring for comments
+    """
+    print("Hello")
+result = name_of_function()
+print(result)
+
+# Example function
+def name_of_function(name):
+    """
+    prints out the name
+    """
+    print("hello"+name)
+
+# Example function 2
+def add_function(num1,num2):
+    return num1+num2
+    result = add_function(1,2)
+print(result)
